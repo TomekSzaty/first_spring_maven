ed0fc97fb4ff2de4640d4398cbc10eb6ead76ce0
TomekSzaty <75204970+TomekSzaty@users.noreply.github.com>
2021-11-05 15:39:20 +0100

    Web_HelloApp with Maven

---
+++ src/test/java/com/example/first_spring_maven/FirstSpringMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.first_spring_maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
